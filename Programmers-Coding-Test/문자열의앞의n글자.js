c9cb928e51cb01e7ba55686f121ff5d0aee0657d
Dohangang <121237964+Dohangang@users.noreply.github.com>
2023-06-20 16:42:00 +0900

    코테 문자열의앞의의n글자

---
+++ Programmers-Coding-Test/문자열의앞의n글자.js
@@ -0,0 +1,25 @@
+/*
+문제 설명:
+문자열 my_string과 정수 n이 매개변수로 주어질 때, 
+my_string의 앞의 n글자로 이루어진 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+제한사항:
+📌 my_string은 숫자와 알파벳으로 이루어져 있습니다.
+📌 1 ≤ my_string의 길이 ≤ 1,000
+📌 1 ≤ n ≤ my_string의 길이
+*/
+
+function solution(my_string, n) {
+  const reg = /^[a-zA-Z0-9]*$/;
+  if (!reg.test(my_string)) {
+    return "regular expression error";
+  }
+  if (my_string.length < 1 || 1000 < my_string.length) {
+    return "length error";
+  }
+  if (my_string < n || n < 1) {
+    return "length error";
+  }
+
+  return my_string.slice(0, n);
+}
