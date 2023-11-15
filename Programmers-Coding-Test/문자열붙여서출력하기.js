edd55ab0c55b3366f1bb51cab5d57970f52c9dc6
Dohangang <121237964+Dohangang@users.noreply.github.com>
2023-11-15 15:33:24 +0900

    코테 문자열붙여서출력하기

---
+++ Programmers-Coding-Test/문자열붙여서출력하기.js
@@ -0,0 +1,25 @@
+/*
+문제 설명:
+두 개의 문자열 str1, str2가 공백으로 구분되어 입력으로 주어집니다.
+입출력 예와 같이 str1과 str2을 이어서 출력하는 코드를 작성해 보세요.
+
+제한사항:
+📌 1 ≤ str1, str2의 길이 ≤ 10
+*/
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  const result = input[0] + input[1];
+  console.log(result);
+});
+
+// 기본 제공 코드가 이상하다.
