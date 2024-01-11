452d39f682faaab5a25bf4b49e4dfbbe9e5f358d
Dohangang <121237964+Dohangang@users.noreply.github.com>
2024-01-11 15:35:14 +0900

    코테 x만큼간격이있는n개의숫자 수정

--- Programmers-Coding-Test/x만큼간격이있는n개의숫자.js
+++ Programmers-Coding-Test/x만큼간격이있는n개의숫자.js
@@ -16,3 +16,25 @@ function solution(x, n) {
   }
   return answer;
 }
+
+// function solution(x, n) {
+//   let answer = [];
+
+//   if (x > 0) {
+//     for (let i = x; i <= x * n; i = i + x) {
+//       answer.push(i);
+//     }
+//   } else if (x < 0) {
+//     for (let i = x; x * n <= i; i = i + x) {
+//       answer.push(i);
+//     }
+//   }
+
+//   if (x === 0) {
+//     for (let i = 0; i < n; i++) {
+//       answer.push(0);
+//     }
+//   }
+
+//   return answer;
+// }
