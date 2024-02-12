e9313789dcc107c837a46253baec0d9bcb3d2712
Dohangang <121237964+Dohangang@users.noreply.github.com>
2024-02-12 23:40:34 +0900

    코테 문자열을정수로변환하기

---
+++ Programmers-Coding-Test/문자열을정수로변환하기.js
@@ -0,0 +1,21 @@
+/*
+문제 설명:
+숫자로만 이루어진 문자열 n_str이 주어질 때, 
+n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+제한사항:
+📌 1 ≤ n_str ≤ 5
+📌 n_str은 0부터 9까지의 정수 문자로만 이루어져 있습니다.
+*/
+
+function solution(n_str) {
+  return +n_str;
+}
+
+// function solution(n_str) {
+//   return Number(n_str);
+// }
+
+// function solution(n_str) {
+//   return parseInt(n_str);
+// }
