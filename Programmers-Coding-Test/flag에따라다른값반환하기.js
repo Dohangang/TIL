be22bdca1b8703df5dccb83a6312bfef5c686a2d
Dohangang <121237964+Dohangang@users.noreply.github.com>
2024-02-13 14:13:20 +0900

    코테 flag에따라다른값반환하기

---
+++ Programmers-Coding-Test/flag에따라다른값반환하기.js
@@ -0,0 +1,12 @@
+/*
+문제 설명:
+두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, 
+flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+
+제한사항:
+📌 -1,000 ≤ a, b ≤ 1,000
+*/
+
+function solution(a, b, flag) {
+  return flag ? a + b : a - b;
+}
