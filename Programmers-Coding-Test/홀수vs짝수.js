ce505dd019509b5347b3688aa7ea8e3a10672ea3
Dohangang <121237964+Dohangang@users.noreply.github.com>
2024-05-08 15:36:54 +0900

    코테 n개간격의원소들

--- Programmers-Coding-Test/홀수vs짝수.js
+++ Programmers-Coding-Test/홀수vs짝수.js
@@ -1,6 +1,8 @@
 /*
 문제 설명:
-정수 리스트 num_list가 주어집니다. 가장 첫 번째 원소를 1번 원소라고 할 때, 홀수 번째 원소들의 합과 짝수 번째 원소들의 합 중 큰 값을 return 하도록 solution 함수를 완성해주세요. 두 값이 같을 경우 그 값을 return합니다.
+정수 리스트 num_list가 주어집니다. 가장 첫 번째 원소를 1번 원소라고 할 때, 
+홀수 번째 원소들의 합과 짝수 번째 원소들의 합 중 큰 값을 return 하도록 solution 함수를 완성해주세요. 
+두 값이 같을 경우 그 값을 return합니다.
 
 
 제한사항:
@@ -23,11 +25,11 @@ function solution(num_list) {
   return odd <= even ? even : odd;
 }
 
-function solution(num_list) {
-  return Math.max(
-    ...num_list.reduce(
-      ([o, e], c, i) => (i % 2 ? [o + c, e] : [o, e + c]),
-      [0, 0]
-    )
-  );
-}
+// function solution(num_list) {
+//   return Math.max(
+//     ...num_list.reduce(
+//       ([o, e], c, i) => (i % 2 ? [o + c, e] : [o, e + c]),
+//       [0, 0]
+//     )
+//   );
+// }
