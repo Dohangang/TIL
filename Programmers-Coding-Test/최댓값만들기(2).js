1cee9bf29513a5d8a1dd33252201379e24c3d97d
Dohangang <121237964+Dohangang@users.noreply.github.com>
2024-05-29 14:49:13 +0900

    코테 최댓값만들기(2)

---
+++ Programmers-Coding-Test/최댓값만들기(2).js
@@ -0,0 +1,27 @@
+/*
+문제 설명:
+정수 배열 numbers가 매개변수로 주어집니다. 
+numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+
+
+제한사항:
+📌 -10,000 ≤ numbers의 원소 ≤ 10,000
+📌 2 ≤ numbers 의 길이 ≤ 100
+*/
+
+function solution(numbers) {
+  numbers.sort((a, b) => a - b);
+
+  return numbers[0] * numbers[1] >
+    numbers[numbers.length - 1] * numbers[numbers.length - 2]
+    ? numbers[0] * numbers[1]
+    : numbers[numbers.length - 1] * numbers[numbers.length - 2];
+}
+
+// function solution(numbers) {
+//   numbers.sort((a, b) => a - b);
+//   return Math.max(
+//     numbers[0] * numbers[1],
+//     numbers[numbers.length - 1] * numbers[numbers.length - 2]
+//   );
+// }
