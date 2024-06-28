08e1e65bbac4c3597e8a0f2840730362715830fd
Dohangang <121237964+Dohangang@users.noreply.github.com>
2024-06-28 14:02:25 +0900

    코테 문자열돌기기

---
+++ Programmers-Coding-Test/문자열돌리기.js
@@ -0,0 +1,35 @@
+/*
+문제 설명:
+문자열 str이 주어집니다.
+문자열을 시계방향으로 90도 돌려서 아래 입출력 예와 같이 출력하는 코드를 작성해 보세요.
+
+입력 #1
+abcde
+
+출력 #1
+a
+b
+c
+d
+e
+
+제한사항:
+📌 1 ≤ str의 길이 ≤ 10
+*/
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = [line];
+}).on("close", function () {
+  const str = input[0];
+  for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+  }
+});
