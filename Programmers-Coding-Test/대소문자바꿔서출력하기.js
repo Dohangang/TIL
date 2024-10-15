91a6358583961879869a16eec8d0dc3d02555331
Dohangang <121237964+Dohangang@users.noreply.github.com>
2024-10-15 19:52:40 +0900

    코테 대소문자바꿔서출력하기

---
+++ Programmers-Coding-Test/대소문자바꿔서출력하기.js
@@ -0,0 +1,36 @@
+/*
+문제 설명:
+영어 알파벳으로 이루어진 문자열 str이 주어집니다. 
+각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
+
+
+제한사항:
+📌 1 ≤ str의 길이 ≤ 20
+      str은 알파벳으로 이루어진 문자열입니다.
+*/
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = [line];
+}).on("close", function () {
+  let str = input[0];
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (char === char.toUpperCase()) {
+      result += char.toLowerCase();
+    } else {
+      result += char.toUpperCase();
+    }
+  }
+
+  console.log(result);
+});
