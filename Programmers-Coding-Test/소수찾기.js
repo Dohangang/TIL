76bcf2d2e54da75244a49880a743fa86166217cf
Dohangang <121237964+Dohangang@users.noreply.github.com>
2024-12-12 16:40:17 +0900

    코테 소수찾기

---
+++ Programmers-Coding-Test/소수찾기.js
@@ -0,0 +1,26 @@
+/*
+문제 설명:
+1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수, solution을 만들어 보세요.
+
+소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
+(1은 소수가 아닙니다.)
+
+
+제한사항:
+📌 n은 2이상 1000000이하의 자연수입니다.
+*/
+
+function solution(n) {
+  const isPrime = Array(n + 1).fill(true);
+  isPrime[0] = isPrime[1] = false;
+
+  for (let i = 2; i * i <= n; i++) {
+    if (isPrime[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+
+  return isPrime.filter(Boolean).length;
+}
