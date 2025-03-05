adfbb716999e59b7e77ff99071781c33aa38ba2b
Dohangang <121237964+Dohangang@users.noreply.github.com>
2025-03-05 18:19:07 +0900

    코테 조건별로분류하며주문상태출력하기 sql

---
+++ Programmers-Coding-Test/조건별로분류하며주문상태출력하기.sql
@@ -0,0 +1,31 @@
+/*
+문제 설명:
+다음은 식품공장의 주문정보를 담은 FOOD_ORDER 테이블입니다. FOOD_ORDER 테이블은 다음과 같으며 
+ORDER_ID, PRODUCT_ID, AMOUNT, PRODUCE_DATE, IN_DATE,OUT_DATE,FACTORY_ID, WAREHOUSE_ID는 
+각각 주문 ID, 제품 ID, 주문양, 생산일자, 입고일자, 출고일자, 공장 ID, 창고 ID를 의미합니다.
+
+Column name	      Type	         Nullable   
+ORDER_ID	        VARCHAR(10)	   FALSE
+PRODUCT_ID      	VARCHAR(5)	   FALSE
+AMOUNT            NUMBER	       FALSE
+PRODUCE_DATE	    DATE	         TRUE
+IN_DATE	          DATE	         TRUE
+OUT_DATE	        DATE	         TRUE
+FACTORY_ID      	VARCHAR(10)	   FALSE
+WAREHOUSE_ID	    VARCHAR(10)    FALSE
+
+FOOD_ORDER 테이블에서 2022년 5월 1일을 기준으로 주문 ID, 제품 ID, 출고일자, 출고여부를 조회하는 SQL문을 작성해주세요. 
+출고여부는 2022년 5월 1일까지 출고완료로 이 후 날짜는 출고 대기로 미정이면 출고미정으로 출력해주시고, 결과는 주문 ID를 기준으로 오름차순 정렬해주세요.
+
+제한사항:
+📌 
+*/
+
+SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE, '%Y-%m-%d') AS OUT_DATE,
+CASE
+  WHEN OUT_DATE >= '2022-05-02' THEN '출고대기'
+  WHEN OUT_DATE IS NULL THEN '출고미정'
+  ELSE '출고완료'
+END AS 출고여부
+FROM FOOD_ORDER
+ORDER BY ORDER_ID ASC;
