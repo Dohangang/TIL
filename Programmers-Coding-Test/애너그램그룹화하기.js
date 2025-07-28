1b825f3338c0607ba15180ff1948c899b49d69fc
Dohangang <121237964+Dohangang@users.noreply.github.com>
2025-07-28 17:12:15 +0900

    map 객체 복습, 배열 메소드 원본 훼손 구분 복습

--- Programmers-Coding-Test/애너그램그룹화하기.js
+++ Programmers-Coding-Test/애너그램그룹화하기.js
@@ -26,3 +26,22 @@ function groupAnagrams(strs) {
 
   return Array.from(map.values());
 }
+
+/*
+Map 객체 : 키-값 쌍을 저장하는 자료구조, 키에 모든 자료형을 쓸 수 있음, 추가한 순서를 보존
+map.set('name', 'Alice');  키-값 쌍을 추가 및 업데이트
+map.get(''name') 키에 연결된 값을 반환
+map.has('name') 특정 키가 존재하는지 확인 TF
+map.delete('name') 특정 키-값 쌍을 삭제
+map.clear() 맵 안의 모든 요소를 제거합니다.
+map.size 저장된 키-값 쌍의 개수를 반환
+.keys()  .values()  .entries()  모든 키 모든 값 모든 키-값배열을 순서대로 맵 반환
+
+Array.from("hello");           // ['h', 'e', 'l', 'l', 'o']
+Array.from(new Set([1, 2, 3])); // [1, 2, 3]
+Array.from([1, 2, 3], x => x * 2); // [2, 4, 6]
+
+배열 원본 변경 O / X
+✅ 변경하지 않음	map, filter, slice, concat, reduce, flat, find, includes, join, Array.from
+❗ 원본 변경	push, pop, shift, unshift, splice, sort, reverse, fill
+*/
