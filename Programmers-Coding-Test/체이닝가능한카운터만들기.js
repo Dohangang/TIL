55de415f8960d80739d7cf2c8ca5adf6d79a55c4
Dohangang <121237964+Dohangang@users.noreply.github.com>
2025-08-15 14:21:11 +0900

    클로저 스코프 개념 설명, this의 활용

--- Programmers-Coding-Test/체이닝가능한카운터만들기.js
+++ Programmers-Coding-Test/체이닝가능한카운터만들기.js
@@ -36,3 +36,17 @@ function createCounter() {
 
   return counterObject;
 }
+
+/*
+1. 클로저를 이용한 정보 은닉 
+함수가 실행된 뒤에도 내부 변수 value는 사라지지 않는다. 내부 변수는 메서드들을 통해서만 접근 가능하다.
+-> 외부에서 직접적인 변수 수정을 막을 수 있는 방법. typescript를 쓰면 java처럼 지정해버릴 수 있음
+
+  TypeScript `private` 클래스 밖에서 쓰면 에러를 띄워줘. 라는 규칙에 가깝다.
+  JavaScript 클로저: 바깥 스코프에서는 안쪽 스코프의 변수에 절대로 접근할 수 없다. 라는 물리적 격리에 가깝다.
+  value 변수는 '스코프' 라는 공간에 살고, '클로저' 는 그 공간을 계속 참조할 수 있도록 다리를 놓아주는 '기능' 또는 '현상' 
+
+2. `return this`를 활용한 메서드 체이닝
+this는 counterObject를 나타낸다(호출한 객체). 
+return this를 통해 객체 자체가 반환되므로 그 뒤에 increment getValue와 같은 메소드를 연이어 쓸 수 있다. 
+*/
