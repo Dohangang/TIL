d0121fb858ee26adb085f432adff9a4f0e98055e
Dohangang <121237964+Dohangang@users.noreply.github.com>
2025-08-19 18:42:50 +0900

    유클리드 호제법 오랜만에 복습

--- Programmers-Coding-Test/분수의덧셈.js
+++ Programmers-Coding-Test/분수의덧셈.js
@@ -18,3 +18,12 @@ function solution(numer1, denom1, numer2, denom2) {
   let g = gcd(numerator, denominator);
   return [numerator / g, denominator / g];
 }
+
+// --------오랜만에 복습---------
+// 유클리드 호제법 활용  (a=bq+r, b=rq'+r'  ->  a와 b의 최대공약수가 b와 r의 최대공약수와 같다)
+// -> a = bq + r  좌변도 우변도 최대공약수의 곱이다 (a'g = b'gq + r'g,  a'g=a  b'g=b  r'g=r,  g는 최대공약수라고 가정한다.)
+// gcd = Greatest Common Divisor
+// lcm = Least Common Multiple
+
+// 유클리드 호제법을 사용한 gcd공식을 사용했다. 정수론, a=bq+r, gcd(a,b)=gcd(b,r)
+// gcd(a,b)=ax+by 도 있으니 유의할 것
