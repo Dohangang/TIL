1100a5d2795d9b620970c95a06db46b0d96c395c
Dohangang <121237964+Dohangang@users.noreply.github.com>
2025-10-16 19:41:23 +0900

    코테 코딩풀이, 복수 매개변수와 구조 분해 할당

--- Programmers-Coding-Test/가장많이등장한숫자.js
+++ Programmers-Coding-Test/가장많이등장한숫자.js
@@ -26,3 +26,10 @@ function solution(arr) {
 
   return Math.min(...numberOfMaxCount);
 }
+
+// -- ountMap.set(num, (countMap.get(num) || 0) + 1) 설명
+//   키, 값의 쌍을 추가 및 업데이트 하기 위함. 키에 대한 값이 있다면 그 값을 가져오고, 없다면 undefined이기 때문에 0으로 처리한다. 후에 +1을 처리
+
+// -- countMap.entries()는 [Key, value] 형태로 반환한다 -> [ [5, 2], [4, 1], [3, 2] ]
+// .filter(([_, count]) => count === maxCount) 에서 쓰지 않을 매개 변수를 관례적으로 '_'와 같이 표시. 안써주면 두번 째 매개변수로 활용을 못함
+// .map(([num]) => num) 에 대한 이해는 [num] = [5, 2] 과 num = 5 이 같은 의미라는 걸 알면 된다. 구조 분해 할당
